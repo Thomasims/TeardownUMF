6d46bac0a1f99359102d78febcecb816a22a8473
Thomasims <thomasims3@hotmail.fr>
2020-11-17 02:17:01 +0100

    util.shared_channel & util.async_channel

--- mods/core/util.lua
+++ mods/core/util.lua
@@ -81,6 +81,7 @@ do
 end
 
 function util.shared_buffer(name, max)
+	max = max or 64
 	return {
 		_pos_name = name .. ".position",
 		_list_name = name .. ".list.",
@@ -109,6 +110,113 @@ function util.shared_buffer(name, max)
 			ClearKey(self._list_name:sub(1, -2))
 		end
 	}
+end
+
+function util.shared_channel(name, max)
+	max = max or 64
+	local channel = {
+		_buffer = util.shared_buffer(name, max),
+		_offset = 0,
+		_hooks = {},
+		_ready_count = 0,
+		_ready = {},
+		broadcast = function(self, ...)
+			return self:send("", ...)
+		end,
+		send = function(self, realm, ...)
+			self._buffer:push(string.format(
+				",%s,;%s",
+				(type(realm) == "table" and table.concat(realm, ",") or tostring(realm)),
+				util.serialize(...)
+			))
+		end,
+		listen = function(self, callback)
+			if self._ready[callback] ~= nil then return end
+			self._hooks[#self._hooks + 1] = callback
+			self:ready(callback)
+			return callback
+		end,
+		unlisten = function(self, callback)
+			self:unready(callback)
+			self._ready[callback] = nil
+			for i = 1, #self._hooks do
+				if self._hooks[i] == callback then
+					table.remove( self._hooks, i )
+					return true
+				end
+			end
+		end,
+		ready = function(self, callback)
+			if not self._ready[callback] then
+				self._ready_count = self._ready_count + 1
+				self._ready[callback] = true
+			end
+		end,
+		unready = function(self, callback)
+			if self._ready[callback] then
+				self._ready_count = self._ready_count - 1
+				self._ready[callback] = false
+			end
+		end
+	}
+	local local_realm = "," .. (REALM or "") .. ","
+	local function receive(...)
+		for i = 1, #channel._hooks do
+			local f = channel._hooks[i]
+			if channel._ready[f] then
+				f(channel, ...)
+			end
+		end
+	end
+	hook.add("base.tick", name, function(dt)
+		if channel._ready_count > 0 then
+			local last_pos = channel._buffer:pos()
+			if last_pos > channel._offset then
+				for i = math.max(channel._offset, last_pos - max), last_pos - 1 do
+					local message = channel._buffer:get_g(i)
+					local start = message:find(";", 1, true)
+					local realms = message:sub(1, start - 1)
+					if realms == ",," or realms:find(local_realm, 1, true) then
+						receive(util.unserialize(message:sub(start + 1)))
+						if channel._ready_count <= 0 then
+							channel._offset = i + 1
+							return
+						end
+					end
+				end
+				channel._offset = last_pos
+			end
+		end
+	end)
+	return channel
+end
+
+function util.async_channel(channel)
+	local listener = {
+		_channel = channel,
+		_waiter = nil,
+		read = function(self)
+			self._waiter = coroutine.running()
+			if not self._waiter then error("async_channel:read() can only be used in a coroutine") end
+			self._channel:ready(self._handler)
+			return coroutine.yield()
+		end,
+		close = function(self)
+			if self._handler then
+				self._channel:unlisten(self._handler)
+			end
+		end,
+	}
+	listener._handler = listener._channel:listen(function(_, ...)
+		if listener._waiter then
+			local co = listener._waiter
+			listener._waiter = nil
+			listener._channel:unready(listener._handler)
+			return coroutine.resume( co, ... )
+		end
+	end)
+	listener._channel:unready(listener._handler)
+	return listener
 end
 
 do
