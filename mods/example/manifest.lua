cf861d61d90b4d3c4325d18f07063c5bc85ccd63
Thomasims <thomasims3@hotmail.fr>
2020-11-25 19:54:31 +0100

    Updated example

--- mods/example/manifest.lua
+++ mods/example/manifest.lua
@@ -1,5 +1,14 @@
 return {
+	-- Name used in UI (NYI)
 	printname = "Example Mod",
+
+	-- Other mods this mod depends on, this uses the folder name in mods/
 	dependencies = {"framework"},
-	realms = {"hud", "world"}
+
+	-- Realms supported by this mod (menu, loading, splash, hud, terminal, tv, world, ...)
+	-- If you don't know which realms you need, you can simply use {"*"}
+	realms = {"hud", "world"},
+
+	-- Mods marked as disabled will not be loaded
+	disabled = true,
 }
