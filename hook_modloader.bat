d102c3642868240bbf34f5c2b86f143001c34104
Thomas <3007463+Thomasims@users.noreply.github.com>
2020-12-02 12:48:30 +0100

    Update hook_modloader.bat

--- hook_modloader.bat
+++ hook_modloader.bat
@@ -3,20 +3,20 @@
 
 :: Menu-related realms
 echo Installing in Menu-related realms...
-find /c "mods/core/init.lua" data\ui\menu.lua  || ( echo|set /p=" loadfile "mods/core/init.lua" "menu"" >> data\ui\menu.lua )
-find /c "mods/core/init.lua" data\ui\loading.lua  || ( echo|set /p=" loadfile "mods/core/init.lua" "loading"" >> data\ui\loading.lua )
-find /c "mods/core/init.lua" data\ui\splash.lua  || ( echo|set /p=" loadfile "mods/core/init.lua" "splash"" >> data\ui\splash.lua )
+find /c "mods/core/init.lua" data\ui\menu.lua  || ( echo|set /p=";loadfile "mods/core/init.lua" "menu"" >> data\ui\menu.lua )
+find /c "mods/core/init.lua" data\ui\loading.lua  || ( echo|set /p=";loadfile "mods/core/init.lua" "loading"" >> data\ui\loading.lua )
+find /c "mods/core/init.lua" data\ui\splash.lua  || ( echo|set /p=";loadfile "mods/core/init.lua" "splash"" >> data\ui\splash.lua )
 
 :: HUD-related realms
 echo Installing in HUD-related realms...
-find /c "mods/core/init.lua" data\ui\hud.lua  || ( echo|set /p=" loadfile "mods/core/init.lua" "hud"" >> data\ui\hud.lua )
-find /c "mods/core/init.lua" data\ui\terminal.lua  || ( echo|set /p=" loadfile "mods/core/init.lua" "terminal"" >> data\ui\terminal.lua )
-find /c "mods/core/init.lua" data\ui\tv.lua  || ( echo|set /p=" loadfile "mods/core/init.lua" "tv"" >> data\ui\tv.lua )
+find /c "mods/core/init.lua" data\ui\hud.lua  || ( echo|set /p=";loadfile "mods/core/init.lua" "hud"" >> data\ui\hud.lua )
+find /c "mods/core/init.lua" data\ui\terminal.lua  || ( echo|set /p=";loadfile "mods/core/init.lua" "terminal"" >> data\ui\terminal.lua )
+find /c "mods/core/init.lua" data\ui\tv.lua  || ( echo|set /p=";loadfile "mods/core/init.lua" "tv"" >> data\ui\tv.lua )
 
 :: Level-related realms
 echo Installing in Level-related realms...
-find /c "mods/core/init.lua" data\script\main.lua  || ( echo|set /p=" loadfile "mods/core/init.lua" "world"" >> data\script\main.lua )
-find /c "mods/core/init.lua" data\script\heist.lua  || ( echo|set /p=" loadfile "mods/core/init.lua" "heist"" >> data\script\heist.lua )
+find /c "mods/core/init.lua" data\script\main.lua  || ( echo|set /p=";loadfile "mods/core/init.lua" "world"" >> data\script\main.lua )
+find /c "mods/core/init.lua" data\script\heist.lua  || ( echo|set /p=";loadfile "mods/core/init.lua" "heist"" >> data\script\heist.lua )
 
 :: mods.lua creation
 echo Generating mods filesystem...
