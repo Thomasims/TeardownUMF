80d11c472d85b7877b5c44767ceb5c03f589fbd8
Thomasims <thomasims3@hotmail.fr>
2020-12-11 20:09:04 +0100

    Fixed an issue with current_line() causing include() to break

--- mods/umf/injector.lua
+++ mods/umf/injector.lua
@@ -58,6 +58,28 @@ function FileExists(path)
 end
 file = {exists = FileExists}
 
+local knownroots = {}
+local function fixpath(path)
+	local max, maxs = 0
+	for i = 1, #knownroots do
+		local root = knownroots[i]
+		local s = root:find("/", 1, true)
+		while s do
+			local sub = root:sub(s+1)
+			local st, se = path:find(sub, 1, true)
+			if st then
+				if se - st > max then
+					max = st - se
+					maxs = root:sub(1, s + st - 1) .. path:sub(st)
+				end
+				break
+			end
+			s = root:find("/", s + 1, true)
+		end
+	end
+	return maxs or path
+end
+
 function current_line(level)
 	level = (level or 0) + 3
 	local _, line = pcall(error, "-", level)
@@ -67,6 +89,9 @@ function current_line(level)
 		line = "[C]:?"
 	else
 		line = line:sub(1, -4)
+	end
+	if line:sub(1,3) == "..." then
+		line = fixpath(line:sub(4))
 	end
 	return line
 end
@@ -87,7 +112,7 @@ function current_dir(level)
 	if location == "[C]:?" then
 		location = current_line(level + 1)
 	end
-	local file = location:match("^([^:]+)")
+	local file = location:match("^(..[^:]+)")
 	local p = file and file:find("([^/]+)$")
 	if p then file = file:sub(1, p - 1) end
 	return file
@@ -137,29 +162,30 @@ end
 
 print("Initializing modding base for REALM: " .. REALM)
 
-if not UMF_NOMODS then
-	local function canload(manifest)
-		if manifest.disabled then return end
-		for i = 1, #manifest.realms do
-			if manifest.realms[i] == "*" or manifest.realms[i] == REALM then
-				return true
-			end
+local function canload(manifest)
+	if manifest.disabled then return end
+	for i = 1, #manifest.realms do
+		if manifest.realms[i] == "*" or manifest.realms[i] == REALM then
+			return true
 		end
 	end
+end
 
-	local function loadmod(modname, path, manifest)
-		if not canload(manifest) then return end
-		local init, err = loadfile(path .. "/init.lua")
-		if not init then
-			if not unknownfile(err) then printerror(err) end
-			return
-		end
-		-- TODO: dependencies support
-		init()
+local function loadmod(modname, path, manifest)
+	if not canload(manifest) then return end
+	local init, err = loadfile(path .. "/init.lua")
+	if not init then
+		if not unknownfile(err) then printerror(err) end
+		return
 	end
+	-- TODO: dependencies support
+	init()
+end
 
-	for i, modname in ipairs(ListKeys("mods.available")) do
-		local path = GetString(string.format("mods.available.%s.path", modname))
+for i, modname in ipairs(ListKeys("mods.available")) do
+	local path = GetString(string.format("mods.available.%s.path", modname))
+	knownroots[#knownroots + 1] = path
+	if not UMF_NOMODS then
 		local success, manifest = pcall(dofile, path .. "/manifest.lua")
 		if success then
 			pcall(loadmod, modname, path, manifest)
