ad306c374e706e554bb9bcb264a3ec1c6feb9c98
Thomasims <thomasims3@hotmail.fr>
2020-12-23 21:54:03 +0100

    Let original print calls through

--- mods/umf/core/console_backend.lua
+++ mods/umf/core/console_backend.lua
@@ -16,27 +16,29 @@ local function maketext(...)
 	return text
 end
 
+local oldprint = print
 function printcolor(r, g, b, ...)
 	local text = string.format("%f;%f;%f;%s", r, g, b, maketext(...))
 	console_buffer:push(text)
+	return oldprint(...)
 	--Command("console.update")
 end
 
 function print(...)
-	printcolor(1, 1, 1, ...)
+	return printcolor(1, 1, 1, ...)
 end
 
 function printinfo(...)
-	printcolor(0, .6, 1, ...)
+	return printcolor(0, .6, 1, ...)
 end
 
 function warning(msg)
-	printcolor(1, .7, 0, "[WARNING] " .. tostring(msg) .. "\n  " .. table.concat(util.stacktrace(1), "\n  "))
+	return printcolor(1, .7, 0, "[WARNING] " .. tostring(msg) .. "\n  " .. table.concat(util.stacktrace(1), "\n  "))
 end
 printwarning = warning
 
 function printerror(...)
-	printcolor(1, .2, 0, ...)
+	return printcolor(1, .2, 0, ...)
 end
 
 function clearconsole()
