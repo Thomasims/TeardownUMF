95e21a8e8f1b1c9ed33ca26579a94f9e06d22ae4
William Bergh <berghwilliam@gmail.com>
2021-04-21 23:50:58 +0200

    Bugs fixed & more flexibility
    Previously the writeZ attribute was ignored on polygon, prism and sphere drawing; this is now fixed.
    Furthermore I added a way to customize the DrawLine function directly in the renderer.

--- extension/visual.lua
+++ extension/visual.lua
@@ -12,6 +12,7 @@ COLOR_BLUE = {r = 0, g = 0, b = 255/255, a = 1}
 COLOR_VIOLET = {r = 128/255, g = 0, b = 255/255, a = 255/255}
 COLOR_PINK = {r = 255/255, g = 0, b = 255/255, a = 255/255}
 
+
 if DrawSprite then
     function visual.drawsprite(sprite, source, radius, info)
 		local r, g, b, a = 1, 1, 1, 1
@@ -101,23 +102,23 @@ if DrawSprite then
     end
     
     function visual.drawpolygon(transform, quat, radius, rotation, sides, info)
-		local sin, cos, max = math.sin, math.cos, math.max
-		local r, g, b, a = 1, 1, 1, 1
-		local writeZ, additive = true, false
-		if not transform.pos then transform = Transform(transform, quat or QUAT_ZERO) end
 		local points = {}
-		local ratio = max(info.r, info.g, info.b, info.a)
-		local DrawFunction = writeZ and DrawLine or DebugLine
 		local iteration = 1
+		local sqrt, sin, cos, max = math.sqrt, math.sin, math.cos, math.max
+		local r, g, b, a
+		local DrawFunction = DrawLine
+
+		if not transform.pos then transform = Transform(transform, quat or QUAT_ZERO) end
 
 		if info then
-			r = info.r / ratio or r
-			g = info.g / ratio or g
-			b = info.b / ratio or b
-            a = info.a / ratio or a
-			writeZ = info.writeZ ~= nil and info.writeZ or writeZ
-			additive = info.additive ~= nil and info.additive or additive
-			target = info.target or target
+			r = info.r and info.r or 1
+			g = info.g and info.g or 1
+			b = info.b and info.b or 1
+			a = info.a and info.a or max(r, g, b)
+
+			local ratio = max(r, g, b, a)
+			r, g, b, a = r / ratio, g / ratio, b / ratio, a / ratio
+			DrawFunction = info.DrawFunction ~= nil and info.DrawFunction or (info.writeZ == false and DebugLine or DrawLine)
 		end
 		
 		for v = 0, 360, 360 / sides do
@@ -133,23 +134,23 @@ if DrawSprite then
     end
 
 	function visual.drawprism(transform, quat, radius, depth, rotation, sides, info)
-		local sin, cos, max = math.sin, math.cos, math.max
-		local r, g, b, a = 1, 1, 1, 1
-		local writeZ, additive = true, false
-		if not transform.pos then transform = Transform(transform, quat or QUAT_ZERO) end
 		local points = {}
-		local ratio = max(info.r, info.g, info.b, info.a)
-		local DrawFunction = writeZ and DrawLine or DebugLine
 		local iteration = 1
+		local sqrt, sin, cos, max = math.sqrt, math.sin, math.cos, math.max
+		local r, g, b, a
+		local DrawFunction = DrawLine
+
+		if not transform.pos then transform = Transform(transform, quat or QUAT_ZERO) end
 
 		if info then
-			r = info.r / ratio or r
-			g = info.g / ratio or g
-			b = info.b / ratio or b
-            a = info.a / ratio or a
-			writeZ = info.writeZ ~= nil and info.writeZ or writeZ
-			additive = info.additive ~= nil and info.additive or additive
-			target = info.target or target
+			r = info.r and info.r or 1
+			g = info.g and info.g or 1
+			b = info.b and info.b or 1
+			a = info.a and info.a or max(r, g, b)
+
+			local ratio = max(r, g, b, a)
+			r, g, b, a = r / ratio, g / ratio, b / ratio, a / ratio
+			DrawFunction = info.DrawFunction ~= nil and info.DrawFunction or (info.writeZ == false and DebugLine or DrawLine)
 		end
 
 		for v = 0, 360, 360 / sides do
@@ -165,36 +166,36 @@ if DrawSprite then
 	end
 
 	function visual.drawsphere(transform, radius, rotation, samples, info)
-		local sqrt, sin, cos, max = math.sqrt, math.sin, math.cos, math.max
-		local r, g, b, a = 1, 1, 1, 1
-		local writeZ, additive = true, false
-		if not transform.pos then transform = Transform(transform, quat or QUAT_ZERO) end
 		local points = {}
-		local ratio = max(info.r, info.g, info.b, info.a)
-		local DrawFunction = writeZ and DrawLine or DebugLine
+		local sqrt, sin, cos, max = math.sqrt, math.sin, math.cos, math.max
+		local r, g, b, a
+		local DrawFunction = DrawLine
+
+		if not transform.pos then transform = Transform(transform, quat or QUAT_ZERO) end
 
 		if info then
-			r = info.r / ratio or r
-			g = info.g / ratio or g
-			b = info.b / ratio or b
-            a = info.a / ratio or a
-			writeZ = info.writeZ ~= nil and info.writeZ or writeZ
-			additive = info.additive ~= nil and info.additive or additive
-			target = info.target or target
+			r = info.r and info.r or 1
+			g = info.g and info.g or 1
+			b = info.b and info.b or 1
+			a = info.a and info.a or max(r, g, b)
+
+			local ratio = max(r, g, b, a)
+			r, g, b, a = r / ratio, g / ratio, b / ratio, a / ratio
+			DrawFunction = info.DrawFunction ~= nil and info.DrawFunction or (info.writeZ == false and DebugLine or DrawLine)
 		end
 
 		-- Converted from python to lua, see original code https://stackoverflow.com/a/26127012/5459461
 		local points = {}
 		for i = 0, samples do
-			local y = 1 - (i / (samples - 1)) * 2  -- Y goes from 1 to -1
-			local rad = sqrt(1 - y * y)  -- Radius at y
-			local theta = (2.399963229728653 * i) + rotation  -- Golden angle increment
+			local y = 1 - (i / (samples - 1)) * 2
+			local rad = sqrt(1 - y * y)
+			local theta = (2.399963229728653 * i) + rotation
 
 			local x = cos(theta) * rad
 			local z = sin(theta) * rad
 			local point = TransformToParentPoint(transform, Vec(x * radius, y * radius, z * radius))
 
-			DrawFunction(point, VecAdd(point, Vec(0, .005, 0)), r, g, b, a)
+			DrawFunction(point, VecAdd(point, Vec(0, .01, 0)), r, g, b, a)
 			points[i + 1] = point
 		end
 
