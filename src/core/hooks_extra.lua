1aafae79d0cfc571556b473717edd21778d5ff35
Thomasims <thomasims3@hotmail.fr>
2022-03-10 23:33:11 +0100

    Added logical inputs to api hooks & hook.used checks

--- src/core/hooks_extra.lua
+++ src/core/hooks_extra.lua
@@ -35,38 +35,83 @@ local function checkkeys( func, mousehook, keyhook )
 end
 
 local mousekeys = { "lmb", "rmb", "mmb" }
+local logicalinputs = {
+	"up",
+	"down",
+	"left",
+	"right",
+	"interact",
+	"flashlight",
+	"jump",
+	"crouch",
+	"usetool",
+	"grab",
+	"handbrake",
+	"map",
+	"pause",
+	"vehicleraise",
+	"vehiclelower",
+	"vehicleaction",
+	"camerax",
+	"cameray",
+}
 local heldkeys = {}
 
 hook.add( "base.tick", "api.default_hooks", function()
 	if InputLastPressedKey then
-		for i = 1, #mousekeys do
-			local k = mousekeys[i]
-			if InputPressed( k ) then
-				hook.saferun( "api.mouse.pressed", k )
-			elseif InputReleased( k ) then
-				hook.saferun( "api.mouse.released", k )
+		if hook.used( "api.mouse.pressed" ) or hook.used( "api.mouse.released" ) then
+			for i = 1, #mousekeys do
+				local k = mousekeys[i]
+				if InputPressed( k ) then
+					hook.saferun( "api.mouse.pressed", k )
+				elseif InputReleased( k ) then
+					hook.saferun( "api.mouse.released", k )
+				end
 			end
 		end
-		local lastkey = InputLastPressedKey()
-		if lastkey ~= "" then
-			heldkeys[lastkey] = true
-			hook.saferun( "api.key.pressed", lastkey )
-		end
-		for key in pairs( heldkeys ) do
-			if not InputDown( key ) then
-				heldkeys[key] = nil
-				hook.saferun( "api.key.released", key )
-				break
+		if hook.used( "api.input.pressed" ) or hook.used( "api.input.released" ) then
+			for i = 1, #logicalinputs do
+				local k = logicalinputs[i]
+				if InputPressed( k ) then
+					hook.saferun( "api.input.pressed", k )
+				elseif InputReleased( k ) then
+					hook.saferun( "api.input.released", k )
+				end
 			end
 		end
-		local wheel = InputValue( "mousewheel" )
-		if wheel ~= 0 then
-			hook.saferun( "api.mouse.wheel", wheel )
+		if hook.used( "api.key.pressed" ) or hook.used( "api.key.released" ) then
+			local lastkey = InputLastPressedKey()
+			if lastkey ~= "" then
+				heldkeys[lastkey] = true
+				hook.saferun( "api.key.pressed", lastkey )
+			end
+			for key in pairs( heldkeys ) do
+				if not InputDown( key ) then
+					heldkeys[key] = nil
+					hook.saferun( "api.key.released", key )
+					break
+				end
+			end
 		end
-		local mousedx = InputValue( "mousedx" )
-		local mousedy = InputValue( "mousedy" )
-		if mousedx ~= 0 or mousedy ~= 0 then
-			hook.saferun( "api.mouse.move", mousedx, mousedy )
+		if hook.used( "api.mouse.wheel" ) then
+			local wheel = InputValue( "mousewheel" )
+			if wheel ~= 0 then
+				hook.saferun( "api.mouse.wheel", wheel )
+			end
+		end
+		if hook.used( "api.mouse.move" ) then
+			local mousedx = InputValue( "mousedx" )
+			local mousedy = InputValue( "mousedy" )
+			if mousedx ~= 0 or mousedy ~= 0 then
+				hook.saferun( "api.mouse.move", mousedx, mousedy )
+			end
+		end
+		if hook.used( "api.input.camera" ) then
+			local camerax = InputValue( "camerax" )
+			local cameray = InputValue( "cameray" )
+			if camerax ~= 0 or cameray ~= 0 then
+				hook.saferun( "api.input.camera", camerax, cameray )
+			end
 		end
 	elseif InputPressed then
 		checkkeys( InputPressed, "api.mouse.pressed", "api.key.pressed" )
